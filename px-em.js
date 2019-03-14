649ecfffed338184fa1919cea7f30d3e16a30b6c
Roy Barber <hi@roybarber.com>
2019-03-14 16:51:57 +0000

    Update px-em.js

--- px-em.js
+++ px-em.js
@@ -1,7 +1,23 @@
+numi.addUnit({
+    "id": "rem",
+    "phrases": "rem",
+    "baseUnitId": "em",
+    "format" : "rem",
+    "ratio" : 1,
+ });
+
 numi.addFunction({ "id": "pxem", "phrases": "pxem" }, function(values) {
     if (values.length === 1) {
-        return { "double": values[0].double / 16 };
+        return { "double": values[0].double / 16 , "unitId" : "em" };
     } else if(values.length === 2 ) {
-        return { "double": values[0].double / values[1].double };
+        return { "double": values[0].double / values[1].double,  "unitId" : "em"};
+    }
+});
+
+numi.addFunction({ "id": "pxrem", "phrases": "pxrem" }, function(values) {
+    if (values.length === 1) {
+        return { "double": values[0].double / 16 , "unitId" : "rem" };
+    } else if(values.length === 2 ) {
+        return { "double": values[0].double / values[1].double,  "unitId" : "rem"};
     }
 });
